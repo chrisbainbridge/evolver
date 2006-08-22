8a981cf1b2e93e5f18ca7d920bc237d1c454b331
Chris Bainbridge <chris.bainbridge@gmail.com>
2006-08-23 00:49:32 +0100

    Remove ancient comments from source code and cleanup. Add destroy() methods to some classes and fix Sim.__del__ to cleanup memory leaks.

---
+++ refcounts.py
@@ -0,0 +1,30 @@
+# -*- Mode: Python; tab-width: 4 -*-
+
+import sys
+import types
+
+from persistent import Persistent
+
+def get_refcounts():
+    d = {}
+    #sys.modules
+    # collect all classes
+    for m in sys.modules.values():
+        for sym in dir(m):
+            o = getattr (m, sym)
+            if type(o) is types.ClassType or type(o) is types.TypeType:
+            #if hasattr(o, '__class__'):
+                #issubclass(o, Persistent):
+                d[o] = sys.getrefcount (o)
+    # sort by refcount
+    pairs = map (lambda x: (x[1],x[0]), d.items())
+    pairs.sort()
+    pairs.reverse()
+    return pairs
+
+def print_top(n):
+    for n, c in get_refcounts()[:n]:
+        print '%10d %s' % (n, c.__name__)
+
+if __name__ == '__main__':
+    top_100()
